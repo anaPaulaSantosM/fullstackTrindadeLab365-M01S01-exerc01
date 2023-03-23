511ca6fc341f04c6c779bff7bbe265f99172c9b7
Ana Paulo dos Santos Marianni <ana.marianni@DESKTOP-P6LCRPC.zoomtecnologia.com.br>
2023-03-23 09:24:55 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var exec1 = "->Exercício 1: [App SaveCollege] Dados dos alunos"
+console.log(exec1);
+
+var aluno = [
+    {nome: "Ana", altura: 1.65, idade: 38, sexo: "Feminino", sala: "Trindade", matematica: 7},
+    {nome: "Bruna", altura: 1.70, idade: 31, sexo: "Feminino", sala: "Trindade", matematica: 7},
+    {nome: "Jully", altura: 1.68, idade: 35, sexo: "Feminino", sala: "Trindade", matematica: 6},
+    {nome: "Dora", altura: 1.58, idade: 21, sexo: "Feminino", sala: "Trindade", matematica: 8},
+    {nome: "Bernardo", altura: 1.72, idade: 32, sexo: "Masculino", sala: "Trindade", matematica: 5}        
+];
